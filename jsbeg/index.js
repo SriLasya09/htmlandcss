4155d851d5a1bc4abe53a04c15d583cc0a54fff5
SriLasya09 <srilasya2912@gmail.com>
2022-04-27 23:45:02 +0530

    commit

---
+++ jsbeg/index.js
@@ -0,0 +1,14 @@
+/* document.getElementById("count-el").innerText = 5
+let firstBatch = 5
+let secondBatch = 7
+let count =  firstBatch + secondBatch
+console.log(count)
+
+let count = 0
+console.log(count)
+
+var myAge =20
+console.log(myAge)*/
+let count= 5
+count = count + 1
+console.log(count)
